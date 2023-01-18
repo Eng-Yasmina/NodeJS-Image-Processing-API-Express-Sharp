e900198cb080e69888fc640fb2f399bcbf731a44
Eng-Yasmina <aliyasoo70@gmail.com>
2023-01-18 16:58:01 +0200

    add jasmine configuration

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("hello world");
+/* Exress to run server and routes */
+import express, { Request, Response } from 'express';
+
+// instance of the express server
+const app: express.Application = express();
+
+/* Start express server */
+const port = 3000;
+const listenToServer = () => {
+    console.log(`server is running on localhost: ${port}`);
+}
+app.listen(port, listenToServer);
+
+/* Routes */
+/* Use the static files in the client folder to appear on our API (localhost: 3000) */
+app.use(express.static('client'));
+
+export default app;
---
+++ src/test/indexSpec.ts
@@ -0,0 +1,15 @@
+// Supertest to make HTTP requests on the server 
+import supertest from 'supertest';
+// import app to simulate routes
+import app from '..';
+
+// instance of supertest request (on our app's routes)
+const request = supertest(app);
+
+describe('Test endpoint server', () => {
+    it('GET the / static files',async () => {
+        const response = await request.get('/');
+        expect(response.status).toBe(200);
+    })
+
+});
