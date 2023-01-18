f4995d099a70798db00ac3aeef0d9ce3790ecf65
Eng-Yasmina <aliyasoo70@gmail.com>
2023-01-18 17:28:36 +0200

    add middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 /* Exress to run server and routes */
 import express, { Request, Response } from 'express';
+// morgan logger
+import morgan from 'morgan';
+// Helmet to secure my express server
+import helmet from 'helmet';
 
 // instance of the express server
 const app: express.Application = express();
@@ -10,6 +14,14 @@ const listenToServer = () => {
     console.log(`server is running on localhost: ${port}`);
 }
 app.listen(port, listenToServer);
+
+/* MiddleWares to run automatically on recieved HTTP requests */
+// HTTP request logger middleware
+app.use(morgan('common'));
+// HTTP secuirty middleware
+app.use(helmet());
+// middleware to parse incoming post requests
+app.use(express.json());
 
 /* Routes */
 /* Use the static files in the client folder to appear on our API (localhost: 3000) */
